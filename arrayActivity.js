722baeabed2ee7f6f6f319cf79f8d798665a7794
Janadriel <pjanadriel@gmail.com>
2024-10-20 10:26:45 +0800

    Create arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,11 @@
+// Favorite Singers
+let favSingers = ["Bruno Mars", "Harry Styles", "Jroa"];
+console.log("My first favorite singer is", favSingers[0]);
+
+// Favorite Numbers
+let favNumbers = ["31", "11", "12", "30"];
+let mixedArr = ["string", ["otherarray"], 123, true];
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
